22c702ae9f2aba0f6e9c0436be5267d2f15505df
Leandro Pereira <leandrosp991@gmail.com>
2025-07-14 21:33:49 -0300

    Atualizando arquivos do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+//Teste
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("(35)99987-9865", "1100", "111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("(35)98701-6178", "Iphone 12 Pro", "000000", 512);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
